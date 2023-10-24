7a524538245181ab99bf75492b4959f5dd1ae205
seomj <smj100394@gmail.com>
2023-10-24 10:48:06 +0000

    Update image 0.1.8

--- deploy_test.sh
+++ deploy_test.sh
@@ -18,7 +18,7 @@ function create_lb(){
 
   # error
   if [ $? -ne 0 ]; then
-    echo "SA 생성 실패"
+    echo "========== SA 생성 실패 =========="
     #curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Failed","emessage":"SA 생성 실패"}' -H "Content-Type: application/json" $API_ENDPOINT
     exit 1
   fi
@@ -34,7 +34,7 @@ function create_lb(){
 
   # error
   if [ $? -ne 0 ]; then
-    echo "Load Balancer Controller 설치에 실패했습니다."
+    echo "========== Load Balancer Controller 설치에 실패했습니다. =========="
     curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Failed","emessage":"Load Balancer Controller 설치에 실패했습니다."}' -H "Content-Type: application/json" $API_ENDPOINT
     exit 1
   fi
@@ -48,10 +48,10 @@ EKS_EXIST=false
 for EKS in $EKS_LIST; do
   if [ "$EKS" == "$AWS_EKS_NAME" ]; then
     aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name $AWS_EKS_NAME
-    echo "Kubeconfig updated successfully for EKS cluster '$AWS_EKS_NAME'."
+    echo "========== Kubeconfig updated successfully for EKS cluster '$AWS_EKS_NAME'. =========="
     # Error 발생 시
     if [ $? -ne 0 ]; then
-      echo "'$AWS_EKS_NAME'의 kubeconfig로 업데이트할 수 없습니다."
+      echo "========== '$AWS_EKS_NAME'의 kubeconfig로 업데이트할 수 없습니다. =========="
       #curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Failed","emessage":"'$AWS_EKS_NAME'의 kubeconfig로 업데이트할 수 없습니다."}' -H "Content-Type: application/json" $API_ENDPOINT
       exit 1
     fi
@@ -63,7 +63,7 @@ done
 
 # eks가 없다면
 if [ "$EKS_EXIST" == false ]; then
-  echo "'$AWS_EKS_NAME'를 찾을 수 없습니다."
+  echo "========== '$AWS_EKS_NAME'를 찾을 수 없습니다. =========="
   #curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Failed","emessage":"'$AWS_EKS_NAME'를 찾을 수 없습니다."}' -H "Content-Type: application/json" $API_ENDPOINT
   exit 1
 fi
@@ -90,11 +90,11 @@ fi
 LB_STATUS=$(kubectl get deployment -n kube-system aws-load-balancer-controller -o jsonpath='{.status.conditions[?(@.type=="Available")].status}')
 
 if [ "$LB_STATUS" != True ]; then
-  echo "Load balancer controller를 사용할 수 없습니다."
+  echo "========== Load balancer controller를 사용할 수 없습니다. =========="
   #curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Failed","emessage":"Load balancer controller를 사용할 수 없습니다."}' -H "Content-Type: application/json" $API_ENDPOINT
   exit 1
 else
-  echo "Load balancer controller is available."
+  echo "========== Load balancer controller is available. =========="
 fi
 
 
@@ -114,14 +114,14 @@ done
 if [ "$NS_EXIST" == false ]; then
   kubectl create namespace "$NAMESPACE_NAME"
   if [ $? -ne 0 ]; then
-    echo "namespace 생성 실패"
+    echo "========== namespace 생성 실패 =========="
     #curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Failed","emessage":"namespace 생성 실패"}' -H "Content-Type: application/json" $API_ENDPOINT
     exit 1
   else
-    echo "Namespace created successfully."
+    echo "========== Namespace created successfully. =========="
   fi
 else
-  echo "Namespace $NAMESPACE_NAME already exists."
+  echo "========== Namespace $NAMESPACE_NAME already exists. =========="
 fi
 
 
@@ -151,7 +151,7 @@ EOF
 fi
 
 if [ $? -ne 0 ]; then
-  echo "secert 실패"
+  echo "========== secert 실패 =========="
   #curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Failed","emessage":"secert 실패"}' -H "Content-Type: application/json" $API_ENDPOINT
   exit 1
 fi
@@ -212,18 +212,18 @@ EOF
 
 kubectl apply -f service.yaml 
 if [ $? -ne 0 ]; then
-  echo "service 실패"
+  echo "========== service 실패 =========="
   #curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Failed","emessage":"service 실패"}' -H "Content-Type: application/json" $API_ENDPOINT
   exit 1
 fi
 
 kubectl apply -f deployment.yaml 
 if [ $? -ne 0 ]; then
-  echo "deployment 실패"
+  echo "========== deployment 실패 =========="
   #curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Failed","emessage":"deployment 실패"}' -H "Content-Type: application/json" $API_ENDPOINT
   exit 1
 fi
 
 # 작업 종료
-echo "배포 성공!"
+echo "========== 배포 성공! =========="
 #curl -i -X POST -d '{"id":'$ID',"progress":"deployment","state":"Success","emessage":"배포 성공!"}' -H "Content-Type: application/json" $API_ENDPOINT
